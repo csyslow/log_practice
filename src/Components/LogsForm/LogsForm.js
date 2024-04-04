704f5375b45beab1ea1eb1e79d7356de7d738efb
siyu <csyslow@gmail.com>
2024-04-04 15:36:17 -0400

    useState added

--- src/Components/LogsForm/LogsForm.js
+++ src/Components/LogsForm/LogsForm.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card/Card';
 import './LogsForm.css'
 
 const LogsForm = () => {
     //存储form数据
-    let inputDate = '';
-    let inputDesc = '';
-    let inputTime = '';
+    const [inputDate, setInputDate] = useState('');
+    const [inputDesc, setInputDesc] = useState('');
+    const [inputTime, setInputTime] = useState('');
 
     // 当表单发生变化时，获取用户输入内容
     const dateChangeHandler = (event) => {
         // event.target 触发该事件的对象，即原生DOM对象
-        inputDate = event.target.value;
+        setInputDate(event.target.value);
     }
     const descChangeHandler = (event) => {
         // event.target 触发该事件的对象，即原生DOM对象
-        inputDesc = event.target.value;
+        setInputDesc(event.target.value);
     }
     const timeChangeHandler = (event) => {
         // event.target 触发该事件的对象，即原生DOM对象
-        inputTime = event.target.value
+        setInputTime(event.target.value);
     }
 
     //汇总表单数据
@@ -33,6 +33,10 @@ const LogsForm = () => {
             time: +inputTime
         }
         console.log(newLogData)
+        //清空表单，重新渲染
+        setInputDate('');
+        setInputDesc('');
+        setInputTime('');
     }
 
     return (
@@ -40,15 +44,15 @@ const LogsForm = () => {
             <form onSubmit={formSubmitHandler}>
                 <div className='form-item'>
                     <label htmlFor='date'>Date</label>
-                    <input id='date' onChange={dateChangeHandler} type='date' />
+                    <input id='date' value={inputDate} onChange={dateChangeHandler} type='date' />
                 </div>
                 <div className='form-item'>
                     <label htmlFor='desc'>Description</label>
-                    <input id='desc' onChange={descChangeHandler} type='text' />
+                    <input id='desc' value={inputDesc} onChange={descChangeHandler} type='text' />
                 </div>
                 <div className='form-item'>
                     <label htmlFor='time'>Time</label>
-                    <input id='time' onChange={timeChangeHandler} type='text' />
+                    <input id='time' value={inputTime} onChange={timeChangeHandler} type='text' />
                 </div>
                 <div className='form-btn'>
                     <button>Add</button>
